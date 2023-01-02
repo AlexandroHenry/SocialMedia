5d6142db245591314ac6b829f68d6c4a1da70b61
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-02 15:41:12 +0900

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Seungchul Ha on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
