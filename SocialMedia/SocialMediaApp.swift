157be9b39a81f5b263aceb1f007480b926e1d69e
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-02 22:10:02 +0900

    Social Media App - Firebase Register & Login Finished

--- SocialMedia/SocialMediaApp.swift
+++ SocialMedia/SocialMediaApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SocialMediaApp: App {
+    
+    init() {
+        FirebaseApp.configure() // Initializing Firebase
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
--- SocialMedia/View/LoginView.swift
+++ SocialMedia/View/LoginView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+// MARK: For Native SwiftUI Image Picker
+import PhotosUI
+import Firebase
+import FirebaseFirestore
+import FirebaseStorage
+
 struct LoginView: View {
     
     // MARK: User Details
@@ -15,6 +21,18 @@ struct LoginView: View {
     
     // MARK: View Properties
     @State var createAccount: Bool = false
+    
+    // MARK: Firebase Properties
+    @State var showError: Bool = false
+    @State var errorMessage: String = ""
+    
+    @State var isLoading: Bool = false
+    
+    // MARK: User Defulats
+    @AppStorage("user_profile_url") var profileURL: URL?
+    @AppStorage("user_name") var userNameStored: String = ""
+    @AppStorage("user_UID") var userUID: String = ""
+    @AppStorage("log_status") var logStatus: Bool = false
     
     var body: some View {
         VStack(spacing: 10) {
@@ -31,19 +49,19 @@ struct LoginView: View {
                     .textContentType(.emailAddress)
                     .border(1, .gray.opacity(0.5))
                     .padding(.top, 25)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
                 
                 SecureField("Password", text: $password)
                     .border(1, .gray.opacity(0.5))
                 
-                Button("Reset password?", action: {})
+                Button("Reset password?", action: resetPassword)
                     .font(.callout)
                     .fontWeight(.medium)
                     .tint(.black)
                     .hAlign(.trailing)
                 
-                Button {
-                    
-                } label: {
+                Button(action: loginUser){
                     // MARK: Login Button
                     Text("Sign In")
                         .foregroundColor(.white)
@@ -69,9 +87,69 @@ struct LoginView: View {
         }
         .vAlign(.top)
         .padding(15)
+        .overlay {
+            LoadingView(show: $isLoading)
+        }
         .fullScreenCover(isPresented: $createAccount) {
             RegisterView()
         }
+        // MARK: Displaying Alert
+        .alert(errorMessage, isPresented: $showError, actions: {})
+    }
+    
+    func loginUser() {
+        
+        isLoading = true
+        closeKeyboard()
+        
+        Task {
+            do {
+                // With the help of Swift Concurrency Auth can be done with Single Line
+                try await Auth.auth().signIn(withEmail: emailID, password: password)
+                print("User Found")
+                try await fetchUser()
+            } catch {
+                await setError(error)
+            }
+        }
+    }
+    
+    // MARK: If User if found then Fetching User Data From Firestore
+    func fetchUser() async throws {
+        
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+        let user = try await Firestore.firestore().collection("User").document(userID).getDocument(as: User.self)
+        // MARK: UI Updating Must be Run On Main Thread
+        await MainActor.run(body: {
+            // Setting UserDefaults data and Changing App's Auth Status
+            userUID = userID
+            userNameStored = user.username
+            profileURL = user.userProfileURL
+            logStatus = true
+        })
+    }
+    
+    
+    func resetPassword() {
+        Task {
+            do {
+                // With the help of Swift Concurrency Auth can be done with Single Line
+                try await Auth.auth().sendPasswordReset(withEmail: emailID)
+                print("Link Sent")
+            } catch {
+                await setError(error)
+            }
+        }
+    }
+    
+    // MARK: Displaying Errors via Alert
+    func setError(_ error: Error) async {
+        // MARK: UI Must be Updated on Main Thread
+        await MainActor.run(body: {
+            errorMessage = error.localizedDescription
+            showError.toggle()
+            isLoading = false
+        })
     }
 }
 
@@ -84,9 +162,25 @@ struct RegisterView: View {
     @State var userName: String = ""
     @State var userBio: String = ""
     @State var userBioLink: String = ""
+    @State var userProfilePicData: Data?
     
     // MARK: View Properties
     @Environment(\.dismiss) var dismiss
+    
+    // MARK: PhotoPicker Properties
+    @State var showImagePicker: Bool = false
+    @State var photoItem: PhotosPickerItem?
+    
+    @State var showError: Bool = false
+    @State var errorMessage: String = ""
+    
+    @State var isLoading: Bool = false
+    
+    // MARK: UserDefaults
+    @AppStorage("log_status") var logStatus: Bool = false
+    @AppStorage("user_profile_url") var profileURL: URL?
+    @AppStorage("user_name") var userNameStored: String = ""
+    @AppStorage("user_UID") var userUID: String = ""
     
     var body: some View {
         VStack(spacing: 10) {
@@ -106,7 +200,11 @@ struct RegisterView: View {
             // if the content exceeds more than the available space
             
             ViewThatFits {
+                ScrollView(.vertical, showsIndicators: false) {
+                    HelperView()
+                }
                 
+                HelperView()
             }
             
             // MARK: Register Button
@@ -125,34 +223,83 @@ struct RegisterView: View {
         }
         .vAlign(.top)
         .padding(15)
+        .overlay {
+            LoadingView(show: $isLoading)
+        }
+        .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
+        .onChange(of: photoItem) { newValue in
+            // MARK: Extracting UIImage From PhotoItem
+            if let newValue {
+                Task {
+                    do {
+                        guard let imageData = try await newValue.loadTransferable(type: Data.self) else { return }
+                        // MARK: UI Must Be Updated on Main Thread
+                        await MainActor.run(body: {
+                            userProfilePicData = imageData
+                        })
+                        
+                    } catch {}
+                }
+            }
+        }
+        // MARK: Displaying Alert
+        .alert(errorMessage, isPresented: $showError, actions: {})
     }
     
     @ViewBuilder
     func HelperView() -> some View {
         VStack(spacing: 12) {
+            
+            ZStack {
+                if let userProfilePicData, let image = UIImage(data: userProfilePicData) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } else {
+                    Image("NullProfile")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                }
+            }
+            .frame(width: 85, height: 85)
+            .clipShape(Circle())
+            .contentShape(Circle())
+            .onTapGesture {
+                showImagePicker.toggle()
+            }
+            .padding(.top, 25)
+            
             TextField("Username", text: $userName)
                 .textContentType(.emailAddress)
                 .border(1, .gray.opacity(0.5))
-                .padding(.top, 25)
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
             
             TextField("Email", text: $emailID)
                 .textContentType(.emailAddress)
                 .border(1, .gray.opacity(0.5))
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
                 
             SecureField("Password", text: $password)
                 .border(1, .gray.opacity(0.5))
+            
             
             TextField("About You", text: $userBio, axis: .vertical)
                 .frame(minHeight: 100, alignment: .top)
                 .textContentType(.emailAddress)
                 .border(1, .gray.opacity(0.5))
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
             
             TextField("Bio Link (Optional)", text: $userBioLink)
                 .textContentType(.emailAddress)
                 .border(1, .gray.opacity(0.5))
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
 
             Button {
-                
+                registerUser()
             } label: {
                 // MARK: Sign Up Button
                 Text("Sign Up")
@@ -160,8 +307,61 @@ struct RegisterView: View {
                     .hAlign(.center)
                     .fillView(.black)
             }
+            .disableWithOpacity(userName == "" || userBio == "" || emailID == "" || password == "" || userProfilePicData == nil)
             .padding(.top, 10)
         }
+    }
+    
+    func registerUser() {
+        isLoading = true
+        closeKeyboard()
+        Task {
+            do {
+                // Step 1: Creating Firebase Account
+                try await Auth.auth().createUser(withEmail: emailID, password: password)
+                // Step 2 : Uploading Profile Photo Into Firebase Storage
+                guard let userUID = Auth.auth().currentUser?.uid else { return }
+                guard let imageData = userProfilePicData else { return }
+                let storageRef = Storage.storage().reference().child("Profile_Images").child(userUID)
+                let _ = try await storageRef.putDataAsync(imageData)
+                // Step 3 : Downloading Photo URL
+                let downloadURL = try await storageRef.downloadURL()
+                // Step 4 : Creating a User Firestore Object
+                let user = User(username: userName, userBio: userBio, userBioLink: userBioLink, userUID: userUID, userEmail: emailID, userProfileURL: downloadURL)
+                // Step 5 : Saving User Doc into Firestore Database
+                let _ = try Firestore.firestore().collection("User").document(userUID).setData(from: user, completion: { error in
+                    if error == nil {
+                        // MARK: Print Saved Successfully
+                        print("Saved Successfully")
+                        
+                        userNameStored = userName
+                        self.userUID = userUID
+                        profileURL = downloadURL
+                        logStatus = true
+                    }
+                })
+            } catch {
+                // MARK: Deleting Created Account In Case of Failure
+                try await Auth.auth().currentUser?.delete()
+                
+                // MARK: What's this error?
+                // The operation couldn't be completed. (FirebaseStorage. StorageError error 1.)
+                // Because we only enabled Firebase Auth
+                // and did not enable Firebase Storage or Firebase Firestore,
+                
+                await setError(error)
+            }
+        }
+    }
+    
+    // MARK: Displaying Errors via Alert
+    func setError(_ error: Error) async {
+        // MARK: UI Must be Updated on Main Thread
+        await MainActor.run(body: {
+            errorMessage = error.localizedDescription
+            showError.toggle()
+            isLoading = false
+        })
     }
 }
 
@@ -173,6 +373,20 @@ struct LoginView_Previews: PreviewProvider {
 
 // MARK: View Extensions For UI Building
 extension View {
+    
+    // Closing All Active Keyboards
+    // When the Sign in or Sign Up Buttons are pressed, the active keyboard is closed
+    func closeKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    // MARK: Disabling with Opacity
+    func disableWithOpacity(_ condition: Bool) -> some View {
+        self
+            .disabled(condition)
+            .opacity(condition ? 0.6 : 1)
+    }
+    
     func hAlign(_ alignment: Alignment) -> some View {
         self
             .frame(maxWidth: .infinity, alignment: alignment)
